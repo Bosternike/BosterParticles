681d9b2f0ae789dc6d38deaaaa914a4e9e23bc7e
Bosternike <52787084+Bosternike@users.noreply.github.com>
2022-03-23 01:17:26 +0100

    Add files via upload

---
+++ src/main/java/net/Boster/particles/main/gui/CraftCustomGUI.java
@@ -0,0 +1,118 @@
+package net.Boster.particles.main.gui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+public class CraftCustomGUI {
+
+    public static final HashMap<Player, CraftCustomGUI> openedMap = new HashMap<>();
+
+    private final HashMap<Integer, GUIButton> buttons = new HashMap<>();
+
+    private int slots;
+    private String title;
+
+    private CraftGUIActions actions;
+
+    public boolean accessPlayerInventory = false;
+    public List<Integer> accessibleSlots = new ArrayList<>();
+
+    public CraftCustomGUI(int slots, String title) {
+        this.slots = slots;
+        this.title = title;
+    }
+
+    public CraftCustomGUI(int slots) {
+        this(slots, "BosterParticles");
+    }
+
+    public CraftCustomGUI(String title) {
+        this(9, title);
+    }
+
+    public CraftCustomGUI() {
+        this(9, "BosterParticles");
+    }
+
+    public Inventory getGUI() {
+        Inventory gui = Bukkit.createInventory(null, slots, title);
+        if(actions != null) {
+            actions.onInventoryGeneration(gui);
+        }
+        return gui;
+    }
+
+    public void setTitle(String s) {
+        title = s;
+    }
+
+    public void setSlots(int i) {
+        slots = i;
+    }
+
+    public GUIButton getButton(int i) {
+        return buttons.get(i);
+    }
+
+    public void addButton(GUIButton button) {
+        buttons.put(button.getSlot(), button);
+    }
+
+    public void removeButton(int i) {
+        buttons.remove(i);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setOpened(Player p) {
+        onOpen(p);
+        openedMap.put(p, this);
+    }
+
+    public void setClosed(Player p) {
+        onClose(p);
+        openedMap.remove(p);
+    }
+
+    public static CraftCustomGUI getPlayerOpenGUI(Player p) {
+        return openedMap.get(p);
+    }
+
+    public void onClose(Player p) {
+        if(actions != null) {
+            actions.onClose(p);
+        }
+    }
+
+    public void onOpen(Player p) {
+        if(actions != null) {
+            actions.onOpen(p);
+        }
+    }
+
+    public Collection<GUIButton> getButtons() {
+        return buttons.values();
+    }
+
+    public CraftGUIActions getActions() {
+        return actions;
+    }
+
+    public void setActions(CraftGUIActions actions) {
+        this.actions = actions;
+    }
+
+    public boolean checkAccess(int slot) {
+        if(accessibleSlots == null) return false;
+
+        return accessibleSlots.contains(slot);
+    }
+}
---
+++ src/main/java/net/Boster/particles/main/listeners/InventoryListener.java
@@ -0,0 +1,49 @@
+package net.Boster.particles.main.listeners;
+
+import net.Boster.particles.main.gui.CraftCustomGUI;
+import net.Boster.particles.main.gui.GUIButton;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+public class InventoryListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onClose(InventoryCloseEvent e) {
+        Player p = (Player) e.getPlayer();
+
+        if(e.getInventory() == null) return;
+
+        CraftCustomGUI cg = CraftCustomGUI.getPlayerOpenGUI(p);
+        if(cg != null) {
+            cg.setClosed(p);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onClick(InventoryClickEvent e) {
+        Player p = (Player) e.getWhoClicked();
+
+        if(e.getInventory() == null) return;
+        if(e.getClickedInventory() == null) return;
+        if(e.getView().getTitle() == null) return;
+
+        CraftCustomGUI cg = CraftCustomGUI.getPlayerOpenGUI(p);
+        if(cg != null) {
+            if(e.getClickedInventory().getHolder() instanceof Player) {
+                e.setCancelled(!cg.accessPlayerInventory);
+                return;
+            }
+
+            e.setCancelled(!cg.checkAccess(e.getSlot()));
+            GUIButton b = cg.getButton(e.getSlot());
+
+            if(b != null) {
+                b.onClick(p);
+            }
+        }
+    }
+}
