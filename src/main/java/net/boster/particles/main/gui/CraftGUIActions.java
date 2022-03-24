21c7c02f8ba5ffcf38ba4eae06c29dce92eed5ea
Bosternike <52787084+Bosternike@users.noreply.github.com>
2022-03-24 19:59:13 +0100

    Add files via upload

---
+++ src/main/java/net/boster/particles/main/gui/CraftGUIActions.java
@@ -0,0 +1,19 @@
+package net.boster.particles.main.gui;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class CraftGUIActions {
+
+    @SuppressWarnings("PMD.UncommentedEmptyMethodBody")
+    public void onOpen(Player p) {
+    }
+
+    @SuppressWarnings("PMD.UncommentedEmptyMethodBody")
+    public void onClose(Player p) {
+    }
+
+    @SuppressWarnings("PMD.UncommentedEmptyMethodBody")
+    public void onInventoryGeneration(Inventory gui) {
+    }
+}
