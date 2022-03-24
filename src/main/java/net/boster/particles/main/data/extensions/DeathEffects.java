13aaa504cc46ead3f00e8ed06b73db3b265cea46
Bosternike <52787084+Bosternike@users.noreply.github.com>
2022-03-24 19:58:08 +0100

    Delete src/main directory

--- src/main/java/net/boster/particles/main/data/extensions/DeathEffects.java
+++
@@ -1,9 +0,0 @@
-package net.boster.particles.main.data.extensions;
-
-import net.boster.particles.main.data.PlayerData;
-import org.jetbrains.annotations.NotNull;
-
-public interface DeathEffects {
-
-    void onDeath(@NotNull PlayerData data);
-}
