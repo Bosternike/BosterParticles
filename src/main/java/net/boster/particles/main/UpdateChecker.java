db3729639d9e42b01f7595d3df1f45e5236bcd8a
Bosternike <52787084+Bosternike@users.noreply.github.com>
2022-03-26 00:20:11 +0100

    Add files via upload

---
+++ src/main/java/net/boster/particles/main/UpdateChecker.java
@@ -0,0 +1,33 @@
+package net.boster.particles.main;
+
+import net.boster.particles.main.utils.LogType;
+import org.bukkit.Bukkit;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class UpdateChecker {
+
+    private final BosterParticles plugin;
+    private final int resourceId;
+
+    public UpdateChecker(BosterParticles plugin, int resourceId) {
+        this.plugin = plugin;
+        this.resourceId = resourceId;
+    }
+
+    public void getVersion(final Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
+                if (scanner.hasNext()) {
+                    consumer.accept(scanner.next());
+                }
+            } catch (IOException exception) {
+                plugin.log("&7Unable to check for updates: " + exception.getMessage(), LogType.INFO);
+            }
+        });
+    }
+}
---
+++ src/main/java/net/boster/particles/main/data/database/setter/MySQLSetter.java
@@ -0,0 +1,83 @@
+package net.boster.particles.main.data.database.setter;
+
+import net.boster.particles.main.BosterParticles;
+import net.boster.particles.main.data.database.MySqlConnectionUtils;
+import net.boster.particles.main.utils.LogType;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+public class MySQLSetter implements DataSetter {
+
+    @Override
+    public String getName() {
+        return "MySql";
+    }
+
+    @Override
+    public void setUserData(String uuid, String w, String o) {
+        MySqlConnectionUtils.setMySqlUserValue(uuid, w, o);
+    }
+
+    @Override
+    public String getUserData(String uuid, String value) {
+        return MySqlConnectionUtils.getMySqlValue(uuid, value);
+    }
+
+    @Override
+    public FileConfiguration configuration(String uuid) {
+        FileConfiguration c = new YamlConfiguration();
+        String data = getUserData(uuid, "data");
+        if(data != null) {
+            try {
+                c.loadFromString(decode(data));
+            } catch (InvalidConfigurationException e) {
+                BosterParticles.getInstance().log("Could not load " + uuid + "'s configuration!", LogType.ERROR);
+            }
+        }
+        return c;
+    }
+
+    @Override
+    public void save(String uuid, FileConfiguration file) {
+        setUserData(uuid, "data", encode(file.saveToString()));
+    }
+
+    @Override
+    public void deleteUser(String uuid) {
+        MySqlConnectionUtils.deleteUser(uuid);
+    }
+
+    public static String encode(String s) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try {
+            BukkitObjectOutputStream outputStream = new BukkitObjectOutputStream(byteArrayOutputStream);
+            outputStream.writeObject(s);
+            outputStream.close();
+            return Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
+        } catch (IOException e) {
+            return s;
+        }
+    }
+
+    public static String decode(String s) {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(Base64.getDecoder().decode(s));
+        try {
+            BukkitObjectInputStream inputStream = new BukkitObjectInputStream(byteArrayInputStream);
+            String r = inputStream.readObject().toString();
+            inputStream.close();
+            return r;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return s;
+    }
+}
