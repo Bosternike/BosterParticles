c5ccf3009c77afb0213da0a1ef5c1c393be3396a
Bosternike <52787084+Bosternike@users.noreply.github.com>
2022-04-19 19:41:53 +0200

    Delete src/main directory

--- src/main/java/net/boster/particles/main/utils/creator/ItemCreator.java
+++
@@ -1,8 +0,0 @@
-package net.boster.particles.main.utils.creator;
-
-import org.bukkit.inventory.ItemStack;
-
-public interface ItemCreator {
-
-    ItemStack createItem(String s) throws Exception;
-}
